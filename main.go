b24dca7a5ad5a72f3120d4ec7a7113f76887c9a4
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-10-04 14:24:32 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/DaviidSantos/uno-api/router"
+import (
+	"github.com/DaviidSantos/uno-api/config"
+	"github.com/DaviidSantos/uno-api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+	
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
+	// Initialize router
 	router.Initialize()
 }
