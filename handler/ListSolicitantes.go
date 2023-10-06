26e5dd31151e19676c662866cda5beaa2c0d38c4
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-10-06 11:11:59 -0300

    docs: add documentation for List Solicitantes

--- handler/ListSolicitantes.go
+++ handler/ListSolicitantes.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+
+// @BasePath /api/v1
+
+// @Summary List solicitante
+// @Description List all solicitantes
+// @Tags Solicitante
+// @Produce json
+// @Success 200 {object} ListSolicitanteResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /solicitantes [get]
 func ListSolicitantes(ctx *gin.Context) {
 	solicitantes := []schemas.Solicitante{}
 
