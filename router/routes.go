1e7c13980bba685f96cb133eeffd1452fe76630e
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-10-06 00:08:36 -0300

    feat: add swagger configuration

--- router/routes.go
+++ router/routes.go
@@ -1,16 +1,21 @@
 package router
 
 import (
+	"github.com/DaviidSantos/uno-api/docs"
 	"github.com/DaviidSantos/uno-api/handler"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initilizeRoutes(r *gin.Engine) {
 	// Initialize Handler
 	handler.Init()
-
+	
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 	// Defining routes
-	v1 := r.Group("/api/v1")
+	v1 := r.Group(basePath)
 	{
 		v1.GET("/solicitante", handler.ShowSolicitante)
 
@@ -20,4 +25,6 @@ func initilizeRoutes(r *gin.Engine) {
 
 		v1.GET("/solicitantes", handler.ListSolicitantes)
 	}
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
