6813a2ce68201aaae77f58d662a9100f154dd91b
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-10-06 00:07:31 -0300

    feat: add list solicitantes handler

--- handler/ListSolicitantes.go
+++ handler/ListSolicitantes.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/DaviidSantos/uno-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListSolicitantes(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Get Solicitantes",
-	})
+	solicitantes := []schemas.Solicitante{}
+
+	if err := db.Find(&solicitantes).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing solicitantes")
+		return
+	}
+
+	sendSuccess(ctx, "list-solicitantes", solicitantes)
 }
