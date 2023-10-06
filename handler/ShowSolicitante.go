5c2f6f3727223e1274a7ce08be41f6096dd4dab2
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-10-06 00:07:02 -0300

    feat: add show solicitante handler

--- handler/ShowSolicitante.go
+++ handler/ShowSolicitante.go
@@ -3,11 +3,23 @@ package handler
 import (
 	"net/http"
 
+	"github.com/DaviidSantos/uno-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowSolicitante(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Get Solicitante",
-	})
+	cnpj := ctx.Query("cnpj")
+
+	if cnpj == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	solicitante := schemas.Solicitante{}
+	if err := db.First(&solicitante, cnpj).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "solicitante not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-solicitante", solicitante)
 }
